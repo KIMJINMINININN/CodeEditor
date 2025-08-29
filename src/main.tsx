b9a8c1f694ea8ada8d00a4326436ef552e0f84c8
김진민 <kjm9596@douzone.com>
2025-08-29 13:35:41 +0900

    feat: createRoot null 처리 및 tsconfig 수정

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,8 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider } from 'styled-components';
 import App from './App';
 
-const root = createRoot(document.getElementById('root'));
+const container = document.getElementById('root')!;
+const root = createRoot(container);
 const queryClient = new QueryClient();
 const theme = { colors: { primary: '#4f46e5' } };
 
